8a9d13c8b00ad9d1c89250a591b495870981911b
Julien Caussade <julien.caussade@gmail.com>
2024-04-12 08:51:38 +0200

    JAVASCRIPT UPDATE: - Creation of the game's main features (JS).

--- main.js
+++ main.js
@@ -1,85 +1,158 @@
+// TYPE OF GAME BUTTON:
+const typeGameButton = document.querySelector("#type_game");
+typeGameButton.addEventListener("change", typeGameChoose);
 
-{
-    // const buttonStartRestart = document.getElementById("startRestart");
-    // buttonStartRestart.addEventListener("click", chooseTeam);
+// NUMBER OF GAME:
+const gamesNumberDisplay = document.querySelector("#games_number");
+let gamesNumber = 0;
+gamesNumberDisplay.textContent = gamesNumber;
 
-    // console.log(buttonStartRestart);
+// SCORE:
+let redScore = 0;
+let blueScore = 0;
+const redScoreDisplay = document.querySelector("#redScoreDisplay");
+const blueScoreDisplay = document.querySelector("#blueScoreDisplay");
 
-    // const chooseTeam = prompt("Choose your team : Red or Blue");
-    // if (chooseTeam.toLowerCase == "red") {
-
-    // }
-
-    // function startRestart(buttonStartRestart) {
-        
-    // };
-
-    // console.log(buttonStartRestart);
-}
-
-// FONCION TOUR
-// 9 tours de jeu max
-// Chacun son tour (joueurs = red & blue)
-// Les indices 1, 3, 5, 7, 9 correspondent à un joueur sinon ils correspondent à l'autre -> condition
-
-const gameTurns = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (let i = 0; i < gameTurns.length; i++) {
-    if (gameTurns = gameTurns[0]) {
-        const gameBoxes = document.querySelectorAll(".box");
-        for (let i = 0; i < gameBoxes.length; i++) {
-            gameBoxes[i].addEventListener("click", function() {
-                gameBoxes[i].style.backgroundImage = "url(img/circle.svg)";
-            })
-        }
-    } else if (gameTurns = gameTurns[1]) {
-        const gameBoxes = document.querySelectorAll(".box");
-        for (let i = 0; i < gameBoxes.length; i++) {
-            gameBoxes[i].addEventListener("click", function() {
-                gameBoxes[i].style.backgroundImage = "url(img/cross.svg)";
-            })
-        }
+// RESTART GAME:
+const restartButton = document.querySelector("#restart");
+restartButton.addEventListener("click", restart);
+function restart() {
+    for (let i = 0; i < gameBoxes.length; i++) {
+        gameBoxes[i].style.backgroundImage = "";
     }
+    endGameDisplay.style.display = "none";
+    round = 0;
+    redScore = 0;
+    blueScore = 0;
+    gamesNumber = 0;
+    gamesNumberDisplay.textContent = gamesNumber;
+    redScoreDisplay.textContent = redScore;
+    blueScoreDisplay.textContent = blueScore;
+    playerSelection = true;
 }
 
-
-
-// const gameBoxes = document.querySelectorAll(".box");
-// for (let i = 0; i < gameBoxes.length; i++) {
-//     gameBoxes[i].addEventListener("click", function() {
-//         gameBoxes[i].style.backgroundImage = "url(img/circle.svg)";
-//     })
-// };
-
-// console.log(gameBoxes);
-
-{
-    // const cross = document.createElement("img");
-    // cross.src = "img/cross.svg";
-
-    // const circle = document.createElement("img");
-    // circle.src = "img/circle.svg";
-
-    // const pieces = [cross, circle];
-
-    // gameBoxes[i].addEventListener("click", function() {
-    //     gameBoxes[i].appendChild(pieces);
-    // })
+// RELOAD PLAYGROUND:
+function reloadPlayground() {
+    for (let i = 0; i < gameBoxes.length; i++) {
+        gameBoxes[i].style.backgroundImage = "";
+    }
+    endGameDisplay.style.display = "none";
+    round = 0;
 }
 
-{
-    // const circle = ;
-    // const cross = ;
+// END GAME DISPLAY:
+const endGameDisplay = document.querySelector("#alertWins");
+const buttonContinue = document.querySelector("#continue");
+const winningPlayer = document.querySelector("#winningPlayer");
+buttonContinue.addEventListener("click", reloadPlayground);
+endGameDisplay.style.display = "none";
 
-    // boxL1c1.addEventListener("click", function() {
-    //     document.getElementById("l1c1").style.backgroundImage = "url(img/cross.svg)";
-    // })
+// ROUND:
+let round = 0;
+let playerSelection = true;
 
-    // boxL1c2.addEventListenr("click", function() {
-    //     boxL1c2.appendChild(circle);
-    // })
+// PLAYGROUND:
+const gameBoxes = document.querySelectorAll(".box");
 
-    // let pieces = gameBoxes.appendChild(cross);
+// WINNING COMBOS:
+const l1 = document.querySelectorAll("#l1c1, #l1c2, #l1c3");
+const l2 = document.querySelectorAll("#l2c1, #l2c2, #l2c3");
+const l3 = document.querySelectorAll("#l3c1, #l3c2, #l3c3");
+const c1 = document.querySelectorAll("#l1c1, #l2c1, #l3c1");
+const c2 = document.querySelectorAll("#l1c2, #l2c2, #l3c2");
+const c3 = document.querySelectorAll("#l1c3, #l2c3, #l3c3");
+const d1 = document.querySelectorAll("#l1c1, #l2c2, #l3c3");
+const d2 = document.querySelectorAll("#l1c3, #l2c2, #l3c1");
 
-    // const gameCase = document.getElementsByClassName("case");
-    // console.log(gameCase);
-}
+// END GAME FUNCTION:
+function endGame() {
+    const winningCombos = [l1, l2, l3, c1, c2, c3, d1, d2];
+    for (let i = 0; i < winningCombos.length; i++) {
+        if (
+            winningCombos[i][0].style.backgroundImage == 'url("img/cross.svg")' &&
+            winningCombos[i][1].style.backgroundImage == 'url("img/cross.svg")' &&
+            winningCombos[i][2].style.backgroundImage == 'url("img/cross.svg")'
+        ) {
+            endGameDisplay.style.display = "flex";
+            winningPlayer.textContent = "Red";
+            playerSelection = true;
+            redScore++;
+            gamesNumber++;
+        } 
+        else if (
+            winningCombos[i][0].style.backgroundImage == 'url("img/circle.svg")' &&
+            winningCombos[i][1].style.backgroundImage == 'url("img/circle.svg")' &&
+            winningCombos[i][2].style.backgroundImage == 'url("img/circle.svg")'
+        ) {
+            endGameDisplay.style.display = "flex";
+            winningPlayer.textContent = "Blue";
+            playerSelection = true;
+            blueScore++;
+            gamesNumber++;
+        } else if (round == 9) {
+            endGameDisplay.style.display = "flex";
+            winningPlayer.textContent = "No";
+            gamesNumber++;
+        }
+    gamesNumberDisplay.textContent = gamesNumber;
+    redScoreDisplay.textContent = redScore;
+    blueScoreDisplay.textContent = blueScore;
+    }
+};
+
+// TYPE OF GAME FUNCTION:
+function typeGameChoose() {
+    restart();
+    if (typeGameButton.value == "playerVsPlayer") {
+        for (let i = 0; i < gameBoxes.length; i++) {
+            gameBoxes[i].addEventListener("click", function() {
+                if (round <= 9) {
+                    if (gameBoxes[i].style.backgroundImage == false) {
+                        if (playerSelection == true) {
+                            gameBoxes[i].style.backgroundImage = "url(img/cross.svg)";
+                            playerSelection = false;
+                            round++;
+                            endGame();
+                        } else {
+                            gameBoxes[i].style.backgroundImage = "url(img/circle.svg)";
+                            playerSelection = true;
+                            round++;
+                            endGame();
+                        };
+                    }
+                }
+            })
+        };
+    } else if (typeGameButton.value == "playerVsComputer") {
+        if (round <= 9) {
+            if (playerSelection == true) {
+                for (let i = 0; i < gameBoxes.length; i++) {
+                    gameBoxes[i].addEventListener("click", function() {
+                        if (gameBoxes[i].style.backgroundImage == false) {
+                            gameBoxes[i].style.backgroundImage = "url(img/cross.svg)";
+                            playerSelection = false;
+                            round++;
+                            endGame();
+                            if (playerSelection == false) {
+                                computerPlay();
+                            };
+                        }
+                    })
+                }
+            }
+        }
+    };
+};
+
+// MOVE COMPUTER (BLUE):
+function computerPlay() {
+    const moveComputer = Math.floor(Math.random() * gameBoxes.length);
+    if (gameBoxes[moveComputer].style.backgroundImage == false) {
+    gameBoxes[moveComputer].style.backgroundImage = "url(img/circle.svg)";
+    playerSelection = true;
+    round++;
+    endGame();
+    } else {
+        computerPlay();
+    }
+};
